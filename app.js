01422661b2ccdc3f9c46913ea52bd03674ccc159
bombi <bombic3@naver.com>
2022-05-25 11:19:20 +0900

    Feat : input기능 추가

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick () {
+  // const username = loginInput.value;
+  // if (username === "") {
+  //   alert("Please write your name");
+  // } else if (username.length > 15) {
+  //   alert("Your name is too long.")
+  // }
+  // => html form input에서 컨트롤 가능
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
