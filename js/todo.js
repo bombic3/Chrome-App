9749279fcf57e15f63ad2ab8e2f5ceb62608e567
bombi <bombic3@naver.com>
2022-06-08 12:19:34 +0900

    FEAT : todo list 삭제 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -4,13 +4,39 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-// toDo 그리는 함수
+// 삭제 함수
+function deleteToDo (event) {
+  // event에 관한 모든 정보 확인
+  console.log(event);
+  // button이 있는 해당 부모 li
+  console.dir(event.target.parentElement);
+  // 우리가 삭제할 li
+  const li = event.target.parentElement;
+  // li 삭제
+  li.remove();
+}
+
+// toDo 그리는 함수, 지우기 button 추가
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   // span안에 value값 넣기
   span.innerText = newTodo;
+
+  const button = document.createElement("button");
+  button.innerText = "❌";
+
+  // button 클릭하면 지워지는 기능
+  button.addEventListener("click",deleteToDo);
+
+
+  
+  // li 안에 span 넣기
+  li.appendChild(span);
+  
+  // li 안에 button 넣기
+  li.appendChild(button);
+
   // ul 안에 li 넣기
   toDoList.append(li);
 }
