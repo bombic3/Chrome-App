e63ff04f7140e6bc04fdeea1621b3cfbf649d349
bombi <bombic3@naver.com>
2022-06-08 10:53:19 +0900

    FEAT : 배경img 추가

---
+++ js/background.js
@@ -0,0 +1,17 @@
+const images = [
+  "0.jpg",
+  "1.jpg",
+  "2.jpg"
+];
+// img 폴더 안의 파일 이름과 똑같이 써주면 됨
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+// document.createElement("태그이름") => 태그 생성
+
+bgImage.src = `img/${chosenImage}`;
+// img태그의 src 지정
+
+document.body.appendChild(bgImage);
+// html body에 태그 넣기
